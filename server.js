22b1be252a6e707ebbe5af2efc2949d692825a3c
Vahag Torosyan <vahag.torosyan@nrgaming.com>
2023-12-28 11:46:21 +0400

    cors

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const axios = require('axios')
+const cors = require('cors')
+
 const app = express();
 
-app.use('/', createProxyMiddleware({ toProxy:'', target: 'https://mahbet216.com/mahbet_html', changeOrigin: true }));
+app.use(cors());
+
+app.use('/api', createProxyMiddleware({ target: 'https://mahbet.com', changeOrigin: true }));
+
+
+app.get('/', async function(request, res){
+    res.sendFile(__dirname + '/index.html');
+});
 
 
 app.listen(3000);
